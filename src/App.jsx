fb5c7b88f8c90d64c5f4feff26e1b2166c4636a2
amisadman <inboxsadman@gmail.com>
2025-09-28 14:14:49 +0600

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Country from './Components/Countries/Country';
+
+const App = () => {
+  return (
+    <div>
+       <Country></Country>
+    </div>
+  );
+};
+
+export default App;
